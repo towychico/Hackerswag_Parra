fba082c94177f54326ac23dd7d8a6e08dd88cfc2
Lou <lou@towy.tech>
2022-08-06 22:58:52 -0500

    added basic nav Bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './NavBar.css'
+import './fonts/fonts.css'
 import capIcon from './img/icons/cap.png'
 import posterIcon from './img/icons/poster.png'
+import saleIcon from './img/icons/sale.png'
 import shirtIcon from './img/icons/shirt.png'
 import NavBarCategory from "./navBarCategory/NavBarCategory";
 const NavBarComponent = () => {
@@ -9,9 +11,11 @@ const NavBarComponent = () => {
         <nav className="SideNav">
            <div className="StoreLogo"></div>
             <div className="NavBarContentContainer" style={{display: "flex", flexDirection: "column",justifyContent: "space-around"}}>
-               <NavBarCategory text={'T-Shirts'} img ={shirtIcon}></NavBarCategory>
-               <NavBarCategory text={'Accessories'} img ={capIcon}></NavBarCategory>
-               <NavBarCategory text={'Prints'} img ={posterIcon}></NavBarCategory>
+                <NavBarCategory text={'On Sale'} img ={saleIcon}></NavBarCategory>
+                <NavBarCategory text={'T-Shirts'} img ={shirtIcon}></NavBarCategory>
+                <NavBarCategory text={'Accessories'} img ={capIcon}></NavBarCategory>
+                <NavBarCategory text={'Prints'} img ={posterIcon}></NavBarCategory>
+
 
 
             </div>
